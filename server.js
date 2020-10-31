1423f37805db07dba3df8ffab3c60a5463febd26
RaymondJArgueta <50245443+raymondja@users.noreply.github.com>
2020-10-30 20:00:49 -0400

    'master'

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js';
+//import countries from './public/lab_6/countries.js';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -25,9 +26,10 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
+    const data = await fetch ('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    console.log('fetch request data', data);
     res.json(countries)
   });
   
