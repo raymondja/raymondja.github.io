8a989f8744cf385e1b1d5f6ea41d7c91ece8bd4d
RaymondJArgueta <50245443+raymondja@users.noreply.github.com>
2020-10-30 21:53:22 -0400

    'master'

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     
     .post(async(req, res) => {     
       console.log('POST request detected');     
-      const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json%27');     
+      const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');     
       const json = await data.json();     
       res.json(json);   
     });
